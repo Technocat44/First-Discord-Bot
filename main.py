4207e59cd73d6bdda21d0b92d9288d894cbd7343
technocat44 <71046359+Technocat44@users.noreply.github.com>
2022-03-21 16:21:47 +0000

    Added a random get_quote() function that will randomly pick between 5 quotes in a list and response to a user

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import discord
 import os
+import requests # allows us to make an HTTP request to an API
+import json # the data returned from an API will be in json
+import random
+
 TOKEN = os.environ['TOKEN']
 
 # create an instance of a client, The way we connect to discord. 
 
 client = discord.Client()
 
-# this is called when the bot is ready to be used
-
+def get_quote():
+    response = ["You can do it!", "Don't give up", "You got this!", "Believe it!"]
+    randomIndex = random.randint(0, len(response)-1)
+    return response[randomIndex]
 """
 Asynchronous routines are able to “pause” while waiting on their ultimate result and let other routines run in the meantime.
 
 -- a coroutine is a function that can suspend its execution before reaching return, and it can indirectly pass control to another coroutine for some time.
 """
+# this is called when the bot is ready to be used
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -24,6 +31,6 @@ async def on_message(message):
         return 
 
     if message.content.startswith('$hello'):
-        await message.channel.send('Hello!') 
+        await message.channel.send(get_quote()) 
 
 client.run(TOKEN)
