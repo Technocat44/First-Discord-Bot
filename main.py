e2512d25edaf7009fe901e6d1d9c3cdeb885991f
technocat44 <71046359+Technocat44@users.noreply.github.com>
2022-03-21 16:10:28 +0000

    Set up the simple bot to respond to  messages

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 import discord
+import os
+TOKEN = os.environ['TOKEN']
 
 # create an instance of a client, The way we connect to discord. 
+
+client = discord.Client()
+
+# this is called when the bot is ready to be used
+
+"""
+Asynchronous routines are able to “pause” while waiting on their ultimate result and let other routines run in the meantime.
+
+-- a coroutine is a function that can suspend its execution before reaching return, and it can indirectly pass control to another coroutine for some time.
+"""
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+# this event is triggered each time a message is recieved But if the message is from the bot we don't want it to response to itself
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return 
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!') 
+
+client.run(TOKEN)
